ea7ebaf02e55ad077fec307792f069dac6c71f49
zzb66666666x <18767066589@163.com>
2022-08-19 22:52:43 -0500

    updated

---
+++ CN/1012. 至少有 1 位重复的数字/sol.py
@@ -0,0 +1,21 @@
+class Solution:
+    def numDupDigitsAtMostN(self, n: int) -> int:
+        s = str(n)
+        @cache
+        def f(i: int, mask: int, is_limit: bool, is_num: bool) -> int:
+            if i == len(s):
+                return int(is_num)
+            res = 0
+            if not is_num:  # 可以跳过当前数位
+                res = f(i + 1, mask, False, False)
+            up = int(s[i]) if is_limit else 9
+            for d in range(0 if is_num else 1, up + 1):  # 枚举要填入的数字 d
+                if mask >> d & 1 == 0:  # d 不在 mask 中
+                    res += f(i + 1, mask | (1 << d), is_limit and d == up, True)
+            return res
+        return n - f(0, 0, True, False)
+
+# 作者：endlesscheng
+# 链接：https://leetcode.cn/problems/numbers-with-repeated-digits/solution/by-endlesscheng-c5vg/
+# 来源：力扣（LeetCode）
+# 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
